f0e594dc64ac2ef39bb51dfa817a0750eec6ab6a
Furqan Azhar <furqanazhar92@gmail.com>
2022-10-24 00:47:12 +0500

    invoke checkDateDiff function on Dates

--- routers/rates.py
+++ routers/rates.py
@@ -1,27 +1,33 @@
+from datetime import date
 import json
 import traceback
 from fastapi import APIRouter, status
 from fastapi.responses import JSONResponse
-from models.rates import Rates
-from utils.helper import convert_response_to_json
+from utils.helper import convert_response_to_json, check_date_difference
 from typing import List
 from database.sqlhelper import Database
 
 router = APIRouter()
 db = Database()
 
+
 @router.get('/rates', response_description='Get daily average rates between origin and destination ports/regions')
-async def get_daily_average_rates(rates: Rates):
+async def get_daily_average_rates(date_from: date, date_to: date, origin: str, destination: str):
     try:
-        payload = {
+        isValidDifference = check_date_difference(date_from, date_to)
+        if not isValidDifference:
+            response_payload = {'error': 'Date_From cannot be greater than Date_To'}
+            return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content=response_payload) 
+
+        response_payload = {
             'message': 'Successfully retrieved resource',
-            'data': convert_response_to_json(rates)
+            'data': convert_response_to_json(None)
         }
-        return JSONResponse(status_code=status.HTTP_200_OK, content=payload)
+        return JSONResponse(status_code=status.HTTP_200_OK, content=response_payload)
+
     except Exception as ex:
-        payload = {
+        response_payload = {
             'message': 'Failed to retrieve resource',
             'error': convert_response_to_json(ex)
         }
-        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content=payload)
-
+        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content=response_payload)
