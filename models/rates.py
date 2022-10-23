5cad4c16c88ae7128c9bb982580cb205b1d3ecc8
Furqan Azhar <furqanazhar92@gmail.com>
2022-10-23 22:56:16 +0500

    added validator for date format

--- models/rates.py
+++ models/rates.py
@@ -1,9 +1,18 @@
 from datetime import datetime
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 
 class Rates(BaseModel):
-    date_from: datetime
-    date_to: datetime
+    dateFrom: datetime
+    dateTo: datetime
     origin: str
     destination: str
+
+    @validator('dateFrom', 'dateTo', pre=True)
+    def check_date_format(cls, values):
+        acceptedFormat = "%Y-%m-%d"
+        isValidDateFrom = bool(datetime.strptime(values["dateFrom"], acceptedFormat))
+        isValidDateTo = bool(datetime.strptime(values["dateTo"], acceptedFormat))
+        if not isValidDateFrom or not isValidDateTo:
+            raise ValueError('Invalid date format. Date format should be YYYY-MM-DD')
+        return values
