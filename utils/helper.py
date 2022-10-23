852289dd210444308666751de42aa01543f4e834
Furqan Azhar <furqanazhar92@gmail.com>
2022-10-24 00:48:07 +0500

    add function to check if dates difference is valid

--- utils/helper.py
+++ utils/helper.py
@@ -1,5 +1,19 @@
 import json
+from datetime import datetime
 
 
 def convert_response_to_json(data):
     return json.loads(json.dumps(data, default=str, indent=2))
+
+def check_date_format(date):
+    try:
+        acceptedFormat = "%Y-%m-%d"
+        return bool(datetime.strftime(date, acceptedFormat)) 
+    except ValueError:
+        return False
+
+def check_date_difference(date_from, date_to):
+    if date_from > date_to:
+        return False
+    else:
+        return True
